9fc563440548fa6f8f680a5c4471ebe55ad13caa
Pedrovitor3 <pedrovitorgouveia123@gmail.com>
2023-12-02 11:55:29 -0300

    refactor : providers

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,7 @@ const TagHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 20px 160px;
+  padding: 12px 24px;
 
   > div {
     display: flex;
@@ -30,13 +30,21 @@ const TagHeader = styled.header`
     justify-content: center;
     gap: 24px;
   }
+
+  @media (min-width: ${props => props.theme.tableBreakpoint}) {
+    padding: 20px 160px;
+  }
 `
 
 const Logo = styled.a`
   color: var(--logo-color);
   font-weight: 400;
-  font-size:40px;
+  font-size:24px;
   line-height: 150%;
+
+  @media (min-width: ${props => props.theme.tableBreakpoint}) {
+    fotn-size: 40px;
+  }
 `
 
 
--- src/components/primaryInput.tsx
+++ src/components/primaryInput.tsx
@@ -5,7 +5,7 @@ import { useFilter } from '@/hooks/useFilter';
 
 
 export const PrimaryInput = styled.input`
-  width: 352px;
+  width: 100%;
   border-radius: 8px;
   border: none;
   padding: 10px 16px;
@@ -15,21 +15,31 @@ export const PrimaryInput = styled.input`
 
   font-family: inerit;
   font-weight: 400;
-  font-size: 14px;
-  line-height: 22px;
+  font-size: 12px;
+  line-height: 20px;
   color: var(--text-dark);
+
+  @media (min-width: ${props => props.theme.tableBreakpoint}) {
+    font-size: 14px;
+    line-height: 22px;
+
+  }
 
   `
 
   const InputContainer = styled.div`
     position: relative;
-    width: 352px;
+    width: 250px;
 
     svg {
       position: absolute;
       right: 20px;
       top: 50%;
       transform: translateY(-50%);
+    }
+
+    @media (min-width: ${props => props.theme.tableBreakpoint}) {
+      width: 352px;
     }
   `
   interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
